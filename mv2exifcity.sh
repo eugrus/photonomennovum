b7fa596f23608569f933cea6682122656661f26a
Evgeny Fishgalov <eugrus@gmail.com>
2023-04-01 18:07:13 +0200

    Create mv2exifcity.sh

---
+++ mv2exifcity.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Intro and licensing
+if [ $# -eq 0 ]; then
+  echo "This script will prepend the name of a city where the shot was taken to the filenames of JPEG files provided as arguments."
+  echo "https://github.com/eugrus/";
+  echo "The following terms apply: https://creativecommons.org/licenses/by-nc-sa/3.0/de/deed.en"; echo ""
+  exit 1
+fi
+
+# Check for dependencies
+tools=(curl exiftool jq sed)
+missing=()
+for tool in "${tools[@]}"; do
+  command -v $tool > /dev/null || missing+=($tool)
+done
+if [ ${#missing[@]} -gt 0 ]; then
+  echo "To run this script install the following missing tools: ${missing[@]}"
+  exit 1
+fi
+
+# Loop through files provided as arguments to the script
+for file in "$@"; do
+	
+    # Check if the file is a JPEG
+    if file --mime-type "$file" | grep -q jpeg; then
+    
+        echo Processing "$file"	
+
+        # Extract, parse and print the geographic coordinates from the JPEG
+        lat=$(exiftool -n -gpslatitude "$file" | sed -e 's/^GPS Latitude\s*:\s*//g')
+        echo Latitude: "$lat"
+        lon=$(exiftool -n -gpslongitude "$file" | sed -e 's/^GPS Longitude\s*:\s*//g')
+        echo Longitude: "$lon"
+
+        # If the geo-coordinates are present, use them to find the city
+        if [ -n "$lat" ]; then
+        
+            # Use the OpenStreetMap Nominatim API to reverse geocode: https://nominatim.org/release-docs/latest/api/Reverse/
+            url="https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${lat}&lon=${lon}"
+            city=$(curl -s "$url" | jq -r '.address.city')
+            echo The city is "$city"
+
+            # If a city was found, rename the file with the city name prepended to the original name
+            if [ -n "$city" ]; then
+                newname="${city} ${file}"
+                mv "$file" "$newname"
+                echo "Renamed $file to $newname"
+            fi
+        fi
+    else
+        echo Skipping "$file" as it is not a JPEG file
+    fi
+done
